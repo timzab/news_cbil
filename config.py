0c858b4a6ec94ad5887ed53d44629afbeb6457a3
timzab <a_t_i@mail.ru>
2022-11-12 20:50:24 +0300

    cbiljn 20221112

---
+++ config.py
@@ -0,0 +1,18 @@
+# Параметры из my.telegram.org
+#api_id = "22992259"
+#api_hash = "0ec562e3d0e1c50b091778ef3aacfed7"
+# для +79104098836
+
+api_id = "20179441"
+api_hash = "a4eeb5fbede9c7679bfabd74538d6209"
+# для ++79778946511
+
+# Бот из @BotFather
+#bot_token = "5739893852:AAHJJYmrV9w0Bv9Z8Cvtj1VsZqzI0Emk_Ks" customsonline
+bot_token = "5564458304:AAG1p5AeEergdrbjzSwpa5nG6UcOhn9Yedg"
+
+# id канала куда будут сливаться все новости
+#gazp_chat_id = -1001713518651
+
+#gazp_chat_id = -1001898914053 customsonline
+gazp_chat_id = -1001850446143  #cbil
